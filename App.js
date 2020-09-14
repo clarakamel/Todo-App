1bb2e62a8e92154e18fd95067839dc949dd44b46
clarakamel <clara_atef@hotmail.com>
2020-09-14 14:22:07 +0200

    -a

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+// import { useDimensions } from "@react-native-community/hooks";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+// import ViewImageScreen from "./app/screens/ViewImageScreen";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
+  /*creating an array of Todos*/
+  const [todos, setTodos] = useState([
+    { text: "Buy coffee", key: "1" },
+    { text: "Create an app", key: "2" },
+    { text: "Go to bed", key: "3" },
+  ]);
+
+  /*Function responsible for deleting Todos*/
+  const deleteText = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+/* Function responsible for validating the input data*/
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random.toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("Error!", "Todos must be at least 3 characters long", [
+        { text: "Understood", onPress: () => console.log("Yes Pressed") },
+        {
+          text: "Not Understood!",
+          onPress: () => console.log("No Pressed"),
+        },
+      ]);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} deleteText={deleteText} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  constainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    // flex: 1,
+    marginTop: 20,
   },
 });
